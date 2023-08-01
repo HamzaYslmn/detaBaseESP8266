a99d6fe7de2e6109131c462807e1ed32e8d1f1a9
Hamza Yeşilmen <78810304+HamzaYslmn@users.noreply.github.com>
2023-08-01 10:00:20 +0300

    Create esp8266PutExample.ino

---
+++ esp8266PutExample.ino
@@ -0,0 +1,62 @@
+#include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecureBearSSL.h>
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+
+  // Replace with your actual project ID and base name
+  const char* detaID = "a0uhqqtu49y";
+  const char* detaBaseName = "LOG";
+
+  // Build the URL for the base
+  String url = "https://database.deta.sh/v1/" + String(detaID) + "/" + String(detaBaseName) + "/items";
+
+  // Replace with your actual Data Key (X-API-Key)
+  String api_key = "a0abcyxz_aSecretValue";
+
+  // Create a JSON buffer
+  const size_t capacity = JSON_ARRAY_SIZE(2) + 2 * JSON_OBJECT_SIZE(3);
+  DynamicJsonDocument doc(capacity);
+
+  // Create an array of items
+  JsonArray items = doc.createNestedArray("items");
+
+  // Create the first item and add it to the array
+  JsonObject item1 = items.createNestedObject();
+  item1["key"] = "item1";
+  item1["field1"] = "value1";
+  item1["field2"] = "value2";
+
+  // Serialize the JSON payload to a string
+  String payload;
+  serializeJson(doc, payload);
+
+  // Set the Content-Type header to application/json
+  WiFiClientSecure client;
+  HTTPClient http;
+  client.setInsecure();
+  http.begin(client, url);
+  http.addHeader("Content-Type", "application/json");
+  http.addHeader("X-API-Key", api_key);
+
+  // Send the PUT request with the JSON payload
+  int httpResponseCode = http.PUT(payload);
+
+  // Check the response code
+  if (httpResponseCode > 0) {
+    String response = http.getString();
+    Serial.println("HTTP Response code: " + String(httpResponseCode));
+    Serial.println("Response: " + response);
+  } else {
+    Serial.println("Error on HTTP request");
+  }
+
+  http.end();
+}
+
+void loop() {
+  // Your code here
+}
